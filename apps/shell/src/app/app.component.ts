cca1cfaf921e38d38aac2de4b24ca56f8a994f7c
Houssem-Esprit <houssemeddine.rkaiess@esprit.tn>
2021-07-22 23:09:23 -1200

    check access token claimed from cognito service

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
+import { Cache } from 'aws-amplify';
 
 @Component({
   selector: 'module-federation-ionic-root',
@@ -9,6 +10,7 @@ import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-ampli
 export class AppComponent implements OnInit, OnDestroy {
   user: CognitoUserInterface | undefined;
   authState: AuthState;
+  
   constructor(private ref: ChangeDetectorRef) {}
 
 
@@ -16,10 +18,18 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     return onAuthUIStateChange;
   }
-  ngOnInit() {
+  async ngOnInit()  {
     onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData as CognitoUserInterface;
       this.ref.detectChanges();
-    })  }
+    })  
+  
+  
+     const  federatedInfo = await Cache.getItem('federatedInfo');
+     const { token } = federatedInfo;
+     console.log('user token : ',token);
+  }
+
+
 }
