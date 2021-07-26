78a1a7d40a3b181b6a83a17c136577800c341354
Houssem-Esprit <houssemeddine.rkaiess@esprit.tn>
2021-07-25 22:05:23 -1200

    add business endpoint EV for prod

--- apps/business/src/environments/environment.prod.ts
+++ apps/business/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  PAYMENT_APP_ENDPOINT: "https://master.d24ibg6pnjywzg.amplifyapp.com/"
+  PAYMENT_APP_ENDPOINT: "https://master.d24ibg6pnjywzg.amplifyapp.com/",
+  BUSINESS_APP_ENDPOINT: "https://master.dc8lgauz8jcee.amplifyapp.com/"
 };
