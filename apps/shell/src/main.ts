f4b16d23e034a648780c14e3481a161f7c70a1d9
Houssem-Esprit <houssemeddine.rkaiess@esprit.tn>
2021-07-25 13:44:56 -1200

    fix attempt of amplify.yml 4

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,6 +1,6 @@
 import('./bootstrap')
 	.catch(err => console.error(err));
 	
-	import Amplify, { Auth } from 'aws-amplify';
-	import awsconfig from './aws-exports';
-	Amplify.configure(awsconfig);
+	//import Amplify, { Auth } from 'aws-amplify';
+	//import awsconfig from './aws-exports';
+	//Amplify.configure(awsconfig);
