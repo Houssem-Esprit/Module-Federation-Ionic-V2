8f7f0a40d0e1ef661672931d8f99504dcb5ecd8f
Houssem-Esprit <houssemeddine.rkaiess@confledis.com>
2021-08-24 23:26:23 -1200

    redirect to auth : fix attempt 2

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import {AuthGuardGuard} from '@module-federation-ionic/shared/util/guards';
+import {SharedUtilGuardsModule} from '@module-federation-ionic/shared/util/guards';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AmplifyUIAngularModule ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AmplifyUIAngularModule, SharedUtilGuardsModule ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },AuthGuardGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/shared/util/guards/src/lib/shared-util-guards.module.ts
+++ libs/shared/util/guards/src/lib/shared-util-guards.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthGuardGuard } from './auth-guard.guard';
 
 @NgModule({
   imports: [CommonModule],
+  providers: [AuthGuardGuard],
 })
 export class SharedUtilGuardsModule {}
