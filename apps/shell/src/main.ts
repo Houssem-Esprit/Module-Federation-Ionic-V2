326bc50e5544edd2b8717f8b62d2600d6033bd93
Houssem-Esprit <houssemeddine.rkaiess@confledis.com>
2021-08-26 11:03:23 -1200

    update amplify config

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,8 +1,8 @@
 import('./bootstrap')
 	.catch(err => console.error(err));
 	
-/*	import  { Auth } from 'aws-amplify';
+	import  { Auth } from 'aws-amplify';
 	import awsconfig from './aws-exports';
 	import Storage from '@aws-amplify/storage';
 	Storage.configure(awsconfig);
-	Auth.configure(awsconfig); */
+	Auth.configure(awsconfig); 
